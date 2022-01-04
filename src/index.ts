64f848c74a783ee3401528a32ea6f9cc222803a8
Brenno Nascimento <brenno.nascimento@saudeid.com.br>
2022-01-04 09:47:10 -0300

    validate event type

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,33 @@ const main = async () => {
   app.on('npm-package', handlePublish)
 
   app.listen(PORT || 3000, () => { console.log(`Server listening on port ${PORT || 3000}`) })
+
+  process.on('uncaughtException', error => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(error);
+    process.exit(1)
+  })
+
+  process.on('unhandledRejection', error => {
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(error);
+    process.exit(1)
+  })
+
+  process.on('SIGHUP', () => {
+    console.log('SIGHUP! Shutting down...');
+    process.exit(128 + 1)
+  })
+
+  process.on('SIGINT', () => {
+    console.log('SIGINT! Shutting down...');
+    process.exit(128 + 2)
+  })
+
+  process.on('SIGTERM', () => {
+    console.log('SIGTERM! Shutting down...');
+    process.exit(128 + 3)
+  })
 }
 
 main().catch(error => console.log(error))
--- src/logic/PublishLogic.ts
+++ src/logic/PublishLogic.ts
@@ -1,17 +1,17 @@
 import { Request, Response, NextFunction, Application } from 'express'
 import { NPMPackage } from 'interfaces'
+import { Topics } from '../interfaces/Topics'
 import { app } from "../index"
 import { publish } from '../ports/Kafka'
 
-export const handleMessage = (request: Request, response: Response, next: NextFunction) => {
-  try {
-    app.emit(request.body.event, request.body)
-    response.status(200).json({ status: 'OK!' })
-    next()
-  } catch (error) {
-    console.log(error)
-    next(error)
+export const handleMessage = (request: Request, response: Response, next: NextFunction): Response => {
+  const { event } = request.body
+
+  if (!(event in Topics)) {
+    return response.status(422).json({ error: 'Invalid event' })
   }
+  app.emit(request.body.event, request.body)
+  return response.status(200).json({ status: 'OK!' })
 }
 
 export const handlePublish = async (event: NPMPackage & Application) => {
